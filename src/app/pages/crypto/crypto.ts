e3094c8fdce095836fbd7456a7ddc74f95115faf
darioeliaaa <darioelia61@gmail.com>
2025-11-25 16:05:12 +0100

    Create crypto.ts

---
+++ src/app/pages/crypto/crypto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto',
+  imports: [],
+  templateUrl: './crypto.html',
+  styleUrl: './crypto.css',
+})
+export class Crypto {
+
+}
