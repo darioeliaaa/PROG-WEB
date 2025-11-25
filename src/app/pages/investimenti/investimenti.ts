a0f026b0079a8b464ecabf4bfbe27614ded77c3c
darioeliaaa <darioelia61@gmail.com>
2025-11-25 16:05:28 +0100

    Create investimenti.ts

---
+++ src/app/pages/investimenti/investimenti.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investimenti',
+  imports: [],
+  templateUrl: './investimenti.html',
+  styleUrl: './investimenti.css',
+})
+export class Investimenti {
+
+}
