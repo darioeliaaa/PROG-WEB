95ef83397855b716ab9200668d305847410fa340
darioeliaaa <darioelia61@gmail.com>
2025-11-25 16:04:42 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Movimenti } from './pages/movimenti/movimenti';
+import { Investimenti } from './pages/investimenti/investimenti';
+import { Crypto } from './pages/crypto/crypto';
+import { Login } from './pages/login/login';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+  { path: 'dashboard', component: Dashboard },
+  { path: 'movimenti', component: Movimenti },
+  { path: 'investimenti', component: Investimenti },
+  { path: 'crypto', component: Crypto },
+  { path: 'login', component: Login },
+
+  { path: '**', redirectTo: 'dashboard' }
+];
