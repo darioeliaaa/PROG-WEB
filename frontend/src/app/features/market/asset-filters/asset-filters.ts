aaf81f6bc5b9db9cf5439475cd32c87e9026f52f
darioeliaaa <darioelia61@gmail.com>
2025-12-21 12:24:58 +0100

    Create asset-filters.ts

---
+++ frontend/src/app/features/market/asset-filters/asset-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-filters',
+  imports: [],
+  templateUrl: './asset-filters.html',
+  styleUrl: './asset-filters.css',
+})
+export class AssetFilters {
+
+}
