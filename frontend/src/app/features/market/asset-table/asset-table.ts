a0e91ae490c91f02a7db240c422d367a31b7edd9
darioeliaaa <darioelia61@gmail.com>
2025-12-21 12:25:08 +0100

    Create asset-table.ts

---
+++ frontend/src/app/features/market/asset-table/asset-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-table',
+  imports: [],
+  templateUrl: './asset-table.html',
+  styleUrl: './asset-table.css',
+})
+export class AssetTable {
+
+}
