2e90ecc896933a2d370562c1b24e2af1cd41ae73
darioeliaaa <darioelia61@gmail.com>
2025-12-21 12:25:22 +0100

    Create movimenti.ts

---
+++ frontend/src/app/features/movimenti/movimenti.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimenti',
+  imports: [],
+  templateUrl: './movimenti.html',
+  styleUrl: './movimenti.css',
+})
+export class Movimenti {
+
+}
