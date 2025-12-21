eb0118e665bf7b656c22ff23fe8f879dd15edd0f
darioeliaaa <darioelia61@gmail.com>
2025-12-21 12:24:43 +0100

    Create investment-summary.ts

---
+++ frontend/src/app/features/dashboard/investment-summary/investment-summary.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-summary',
+  imports: [],
+  templateUrl: './investment-summary.html',
+  styleUrl: './investment-summary.css',
+})
+export class InvestmentSummary {
+
+}
