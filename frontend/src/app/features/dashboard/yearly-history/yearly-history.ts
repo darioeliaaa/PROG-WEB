3a42c0954f0720eceebfff0f71155abbf3ffa46c
darioeliaaa <darioelia61@gmail.com>
2025-12-21 12:24:50 +0100

    Create yearly-history.ts

---
+++ frontend/src/app/features/dashboard/yearly-history/yearly-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yearly-history',
+  imports: [],
+  templateUrl: './yearly-history.html',
+  styleUrl: './yearly-history.css',
+})
+export class YearlyHistory {
+
+}
