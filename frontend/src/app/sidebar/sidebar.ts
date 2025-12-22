1547941c0abd02ced14802c286acbc7af96cbd27
darioeliaaa <darioelia61@gmail.com>
2025-12-22 17:54:27 +0100

    Update sidebar.ts

--- frontend/src/app/sidebar/sidebar.ts
+++ frontend/src/app/sidebar/sidebar.ts
@@ -4,10 +4,9 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive], // Serve per far funzionare i link del menu
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class SidebarComponent {
-  // La classe DEVE chiamarsi esattamente SidebarComponent ed avere 'export' davanti
 }
