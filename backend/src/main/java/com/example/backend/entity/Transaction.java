f9e40f1caec5cad1e77283648d1787adde49f494
Matteoiazz <matteo.iazzolino@gmail.com>
2026-01-03 15:58:01 +0100

    primi push database

---
+++ backend/src/main/java/com/example/backend/entity/Transaction.java
@@ -0,0 +1,30 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name = "transactions")
+public class Transaction {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String description;
+
+  @Column(nullable = false)
+  private BigDecimal amount;
+
+  private LocalDate date;
+
+  @Enumerated(EnumType.STRING)
+  private TransactionType type; // Ricorda di creare l'Enum!
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+}
