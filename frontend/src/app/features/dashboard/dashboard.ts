3c3e8656ca83a4fb7619a762128bba8155bff18a
darioeliaaa <darioelia61@gmail.com>
2026-01-03 14:40:39 +0100

    Update dashboard.ts

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// I tuoi componenti figli
 import { InvestmentSummary } from './investment-summary/investment-summary';
 import { BudgetOverview } from './budget-overview/budget-overview';
 import { YearlyHistory } from './yearly-history/yearly-history';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,22 +15,30 @@ import { YearlyHistory } from './yearly-history/yearly-history';
 export class Dashboard {
 
   dataCorrente: Date = new Date();
+  datiMensili: any = null;
+  transazioniAnnuali: any[] = [];
 
-  // 2. Funzione per formattare il testo (es. "Gennaio 2026")
-  // Usiamo questo getter nell'HTML invece della Pipe per averlo subito in Italiano
+  constructor(private service: TransactionService) {
+    this.aggiornaDati();
+  }
+
   get titoloMese(): string {
     return this.dataCorrente.toLocaleString('it-IT', { month: 'long', year: 'numeric' });
   }
 
-  // 3. Logica per andare avanti e indietro
   cambiaMese(delta: number) {
-    // Creiamo una copia della data (fondamentale per far capire ad Angular che è cambiata)
     const nuovaData = new Date(this.dataCorrente);
-
-    // Aggiunge o toglie il mese (gestisce da solo il cambio anno Dicembre->Gennaio)
     nuovaData.setMonth(nuovaData.getMonth() + delta);
-
-    // Aggiorniamo la variabile
     this.dataCorrente = nuovaData;
+    this.aggiornaDati();
+  }
+
+  aggiornaDati() {
+    const mese = this.dataCorrente.getMonth();
+    const anno = this.dataCorrente.getFullYear();
+
+    this.datiMensili = this.service.getDataByMonth(mese, anno);
+
+    this.transazioniAnnuali = this.service.getDataByYear(anno);
   }
 }
