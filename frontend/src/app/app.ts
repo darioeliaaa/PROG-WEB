2fdf8a8aa4d17d60aa3c92ea57b869ebe0889064
darioeliaaa <darioelia61@gmail.com>
2026-01-03 14:40:21 +0100

    Update app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar'; // (Assumendo che il file sia sidebar.ts)
-// 1. IMPORTA L'HEADER QUI SOTTO
+import { SidebarComponent } from './sidebar/sidebar';
 import { HeaderComponent } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. AGGIUNGI HeaderComponent ALLA LISTA IMPORTS
   imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
