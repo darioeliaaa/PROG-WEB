2c7ca312699e2f5bdf2d7d627940f3c9bce9a5a3
darioeliaaa <darioelia61@gmail.com>
2026-01-04 19:27:39 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
 import { Market } from './features/market/market';
 import { Movimenti } from './features/movimenti/movimenti';
+import { Login } from './features/auth/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
   { path: 'market', component: Market },
-  { path: 'movimenti', component: Movimenti }
+  { path: 'movimenti', component: Movimenti },
+  {path: 'login' , component: Login}
 ];
