1b319bdbd5e72175165ba73ec37aa1a934c4c0de
darioeliaaa <darioelia61@gmail.com>
2026-01-04 22:15:56 +0100

    Update login.ts

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,19 +1,92 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router'; // 1. Importa il Router
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class Login {
 
-  // 2. Inietta il router nel costruttore
-  constructor(private router: Router) {}
+  loginObj: any = {
+    email: '',
+    password: ''
+  };
 
-  // 3. Crea questa funzione
+  registerObj: any = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  errorMessage: string = '';
+  registerMessage: string = '';
+
+  isRegisterError: boolean = false;
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  onLogin() {
+    this.errorMessage = '';
+
+    if (this.loginObj.email.trim() == '' || this.loginObj.password.trim() == '') {
+      this.errorMessage = 'Compila tutti i campi per accedere!';
+      return;
+    }
+
+    this.http.post('http://localhost:8080/api/users/login', this.loginObj).subscribe({
+      next: (res: any) => {
+        console.log('Login riuscito!', res);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          this.errorMessage = 'Email o Password errati';
+        } else {
+          this.errorMessage = 'Errore di connessione.';
+        }
+      }
+    });
+  }
+
+  onRegister() {
+    this.registerMessage = '';
+    this.isRegisterError = false;
+
+    if (!this.registerObj.username ||
+      !this.registerObj.email ||
+      !this.registerObj.password) {
+
+      this.registerMessage = 'Tutti i campi sono obbligatori!';
+      this.isRegisterError = true;
+      return;
+    }
+
+    const userToSend = {
+      username: this.registerObj.username,
+      email: this.registerObj.email,
+      password: this.registerObj.password,
+    };
+
+    this.http.post('http://localhost:8080/api/users/register', userToSend).subscribe({
+      next: (res: any) => {
+        this.isRegisterError = false;
+        this.registerMessage = 'Registrazione avvenuta con successo! Fai il login.';
+
+        this.registerObj = { username: '', email: '', password: '' };
+      },
+      error: (err) => {
+        this.isRegisterError = true;
+        this.registerMessage = 'Errore: email già esistente o dati non validi.';
+      }
+    });
+  }
+
   tornaIndietro() {
     this.router.navigate(['/dashboard']);
   }
