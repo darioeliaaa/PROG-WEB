dde17abd930e9476f13ca205855a52668893b99f
MattiaCurcio21 <mttcurcio21@gmail.com>
2026-01-05 19:24:28 +0100

    Update header.ts

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
-  constructor(public router: Router) {}
+  isLoggedIn = false;
+
+  constructor(public router: Router, public userService: UserService) {
+    this.userService.isLoggedIn$.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
