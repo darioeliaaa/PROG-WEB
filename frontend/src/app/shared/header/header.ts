abdef94975d71fc6935939b19b295c4b653b895b
darioeliaaa <darioelia61@gmail.com>
2026-01-06 16:35:23 +0100

    Update header.ts

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, NgIf],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
-  isLoggedIn = false;
+
+  isLoggedIn: boolean = false;
+  showMenu: boolean = false;
 
   constructor(public router: Router, public userService: UserService) {
-    this.userService.isLoggedIn$.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+
+    // 1. Prendi il valore iniziale direttamente dal Service (che ora è corretto subito)
+    this.isLoggedIn = this.userService.isLoggedIn();
+
+    // 2. Iscriviti per i cambiamenti futuri (login/logout)
+    this.userService.isLoggedIn$.subscribe(state => {
+      this.isLoggedIn = state;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  closeMenu() {
+    this.showMenu = false;
   }
 
   logout() {
+    this.showMenu = false;
     this.userService.logout();
     this.router.navigate(['/login']);
   }
