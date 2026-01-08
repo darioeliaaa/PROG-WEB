6b4c7a0a6ed861bfdd7df34ef0bb2d55026afd1e
MattiaCurcio21 <mttcurcio21@gmail.com>
2026-01-08 18:05:50 +0100

    Create profilo.ts

---
+++ frontend/src/app/features/Profilo/profilo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profilo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './profilo.html',
+  styleUrl: './profilo.css',
+})
+export class Profilo {
+
+}
