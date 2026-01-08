a6dcdca609b81c02d59cbe20318b260c9703a15d
MattiaCurcio21 <mttcurcio21@gmail.com>
2026-01-08 18:06:04 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Dashboard } from './features/dashboard/dashboard';
 import { Market } from './features/market/market';
 import { Movimenti } from './features/movimenti/movimenti';
 import { Login } from './features/auth/login/login';
+import { Profilo} from './features/Profilo/profilo';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
   { path: 'market', component: Market },
   { path: 'movimenti', component: Movimenti },
-  {path: 'login' , component: Login}
+  {path: 'login' , component: Login},
+  {path: 'profilo' , component: Profilo}
 ];
