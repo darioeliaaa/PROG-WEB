d250f884ce778f2755e121965dd16a3803d6ecf9
Giammy-bug66 <gmrfil.gf@gmail.com>
2026-01-09 16:31:35 +0100

    Update TransactionRepository.java

--- backend/src/main/java/com/example/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/example/backend/repository/TransactionRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-  // Trova tutti i movimenti di uno specifico utente
-  List<Transaction> findByUserId(Long userId);
+  List<Transaction> findByWalletId(Long walletId); // Per vedere le spese di un solo wallet
 }
