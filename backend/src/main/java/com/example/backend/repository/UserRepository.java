cbe57746f3df5bea9c91f234b21712f3967dba88
Giammy-bug66 <gmrfil.gf@gmail.com>
2026-01-09 16:31:37 +0100

    Update UserRepository.java

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
   // Spring crea automaticamente la query SQL per trovare un utente tramite email
   Optional<User> findByEmail(String email);
+  Optional<User> findByUsername(String username);
 }
