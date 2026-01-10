0f3412746100d9bac60b6571c76a9ebedafb27e0
darioeliaaa <darioelia61@gmail.com>
2026-01-10 16:31:27 +0100

    Update sidebarMarket.ts

--- frontend/src/app/shared/sidebarmarket/sidebarMarket.ts
+++ frontend/src/app/shared/sidebarmarket/sidebarMarket.ts
@@ -9,4 +9,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebarMarket.html',
   styleUrl: './sidebarMarket.css' // Assicurati di usare lo stesso CSS o copiarlo
 })
-export class SidebarMarketComponent {}
+export class SidebarMarketComponent {
+  isMarketOpen: boolean = true; // Imposta su 'false' se lo vuoi chiuso all'avvio
+
+  toggleMarket() {
+    this.isMarketOpen = !this.isMarketOpen;
+  }
+}
