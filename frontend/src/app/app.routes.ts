b069c469d8860756f5cfdd48608eeca907d67c62
MattiaCurcio21 <mttcurcio21@gmail.com>
2026-01-10 17:48:27 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { AssetListComponent } from './features/market/asset-list/asset-list';
 
 import { StockChart } from './features/market/stock-chart/stock-chart';
 import {SettingsComponent} from './settings/settings.component';
+import {WalletComponent} from './features/Wallet/wallet';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -38,5 +39,6 @@ export const routes: Routes = [
   { path: 'movimenti', component: Movimenti },
   { path: 'login' , component: Login },
   { path: 'profilo' , component: Profilo },
-  { path: 'settings', component: SettingsComponent }
+  { path: 'settings', component: SettingsComponent },
+  { path: 'wallet', component: WalletComponent }
 ];
