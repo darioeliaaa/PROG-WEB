d422d330f339be939175530925d66760691fa5c9
darioeliaaa <darioelia61@gmail.com>
2026-01-13 12:25:05 +0100

    Update asset-list.ts

--- frontend/src/app/features/market/asset-list/asset-list.ts
+++ frontend/src/app/features/market/asset-list/asset-list.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // <--- 1. Importalo
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { MarketService, MarketAsset } from '../market.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-asset-list',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,FormsModule],
   templateUrl: './asset-list.html',
   styleUrls: ['./asset-list.css']
 })
@@ -16,6 +17,12 @@ export class AssetListComponent implements OnInit {
   assets: MarketAsset[] = [];
   currentType: string = '';
   loading = false;
+
+  isTradeModalOpen = false;
+  selectedAsset: MarketAsset | null = null;
+  tradeAction: 'BUY' | 'SELL' = 'BUY'; // Azione di default
+  tradeQuantity: number = 1;
+  isProcessing = false; // Per lo spinner sul bottone conferma
 
   constructor(
     private route: ActivatedRoute,
@@ -51,4 +58,52 @@ export class AssetListComponent implements OnInit {
       }
     });
   }
+  openTradePanel(asset: MarketAsset) {
+    this.selectedAsset = asset;
+    this.tradeQuantity = 1; // Reset quantità
+    this.tradeAction = 'BUY'; // Reset azione
+    this.isTradeModalOpen = true;
+  }
+
+  // CHIUDI IL PANNELLO
+  closeTradePanel() {
+    this.isTradeModalOpen = false;
+    this.selectedAsset = null;
+  }
+
+  // IMPOSTA AZIONE (Compra/Vendi)
+  setAction(action: 'BUY' | 'SELL') {
+    this.tradeAction = action;
+  }
+
+  // CALCOLA TOTALE LIVE
+  get estimatedTotal(): number {
+    if (!this.selectedAsset) return 0;
+    return this.selectedAsset.currentPrice * this.tradeQuantity;
+  }
+
+  // ESEGUI TRANSAZIONE
+  confirmTrade() {
+    if (!this.selectedAsset || this.tradeQuantity <= 0) return;
+
+    this.isProcessing = true;
+
+    const request = {
+      assetSymbol: this.selectedAsset.symbol,
+      amount: this.estimatedTotal, // O la quantità, dipende dal tuo backend
+      quantity: this.tradeQuantity,
+      type: this.tradeAction == 'BUY' ? 'ENTRATA' : 'USCITA', // Adatta ai tuoi enum Java
+      priceAtTransaction: this.selectedAsset.currentPrice
+    };
+    console.log('Invio Ordine:', request);
+
+    // Simuliamo una chiamata (Sostituisci con this.marketService.executeTrade(request).subscribe...)
+    setTimeout(() => {
+      alert(`Ordine ${this.tradeAction} eseguito con successo per ${this.selectedAsset?.symbol}!`);
+      this.isProcessing = false;
+      this.closeTradePanel();
+    }, 1500);
+  }
+
+
 }
